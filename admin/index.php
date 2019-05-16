bd7bc8f68a919f162e0de2bf2f5a7dfe7f3446cf
Diksi Erlangga <diksi131@gmail.com>
2019-05-16 09:42:42 +0800

    Salah Push

--- admin/index.php
+++ admin/index.php
@@ -30,8 +30,8 @@
                 <li class="dropdown">
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Menu <span class="caret"></span></a>
                     <ul class="dropdown-menu">
+                      <li><a href="#">Paket Program</a></li>
                       <li><a href="index.php?m=inputdata">Input Data Santri</a></li>
-                      <li><a href="#">Statistic</a></li>
                       <li><a href="#">Reminder Setting</a></li>
                       <li role="separator" class="divider"></li>
                       <li class="dropdown-header">Secondary Action</li>
