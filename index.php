c311ed4907c03b52f1113c178e9455be98bfcfe3
rizky mochtar <mochtarbuchori39@gmail.com>
2019-05-20 15:07:20 +0800

    staging all changes

--- index.php
+++ index.php
@@ -33,19 +33,6 @@ $m = $_GET['m'];
                 <li class="active"><a href="home">Home</a></li>
                 <li><a href="login.php">Login</a></li>
               </ul>
-              <ul class="nav navbar-nav navbar-right">
-                    <li class="dropdown">
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><span class="glyphicon glyphicon-user"></span>&nbsp;Hello 'User'<span class="caret"></span></a>
-                            <ul class="dropdown-menu">
-                              <li><a href="#">Profile</a></li>
-                              <li><a href="#">Projects</a></li>
-                              <li><a href="#">Setting</a></li>
-                              <li role="separator" class="divider"></li>
-                              <li class="dropdown-header">Action</li>
-                              <li><a href="">Sign Out</a></li>
-                            </ul>
-                          </li>
-              </ul>
             </div><!--/.nav-collapse -->
           </div><!--/.container-fluid -->
         </nav>
