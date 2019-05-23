fe42d02f232452a562d4740699c5833c0140925f
Diksi Erlangga <diksi131@gmail.com>
2019-05-23 15:35:29 +0800

    Update Data Snatri

--- admin/datasantri_detail.php
+++ admin/datasantri_detail.php
@@ -1,8 +1,9 @@
 <?php
-    include 'config.php';
-
     $ID = mysqli_real_escape_string($conn, $_GET['ID']);
     echo $ID;
+
+    $query = mysqli_query($conn, "SELECT * FROM tbsantri INNER JOIN tbkelas ON tbkelas.IdKelas=tbsantri.IdKelas WHERE IdSantri='$ID'");
+    $data = mysqli_fetch_object($query);
 ?>
 
 <div class="container">
@@ -16,13 +17,12 @@
                 <img src="../assets/component/profile.png">
             </div>
            <div class="col-md-9">
-                <h4>Nama :</h4>
-                <h4>Kelas :</h4>
-                <h4>NIS  :</h4>
-                <h4>Jenis kelamin :</h4>
-                <h4>Tempat Tanggal lahir :</h4>
-                <h4>Nama Orang Tua/Wali :</h4>
-                <h4>Status Santri :</h4>
+                <h4>Nama : <?php echo $data->NamaLengkap?></h4>
+                <h4>Kelas : <?php echo $data->NamaKelas?></h4>
+                <h4>NIS  : <?php echo $data->NIS?></h4>
+                <h4>Jenis kelamin : <?php echo $data->JenisKelamin?></h4>
+                <h4>Tempat/Tanggal lahir : <?php echo $data->TempatLahir.", ".$data->TglLahir?></h4>
+                <h4>Status Santri : <?php echo $data->StatusSantri?></h4>
            </div>
         </div>
 </div>
